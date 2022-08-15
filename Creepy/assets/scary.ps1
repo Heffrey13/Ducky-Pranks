0befb61f2261088706d5c48590ca2e63dfb448fd
Heffrey13 <91379359+Heffrey13@users.noreply.github.com>
2022-08-15 00:30:56 -0600

    Update scary.ps1

--- Creepy/assets/scary.ps1
+++ Creepy/assets/scary.ps1
@@ -22,7 +22,7 @@ iwr $i -O $env:TMP\i.png
 
 # Download WAV file; replace link to $wav to add your own sound
 
-$wav = "https://github.com/I-Am-Jakoby/hak5-submissions/blob/main/OMG/Payloads/OMG-JumpScare/female_scream.wav?raw=true"
+$wav = "https://github.com/Heffrey13/Ducky-Pranks/blob/main/Creepy/assets/Song/Every%20Breath%20You%20Take.wav?raw=true"
 
 $w = -join($wav,"?dl=1")
 iwr $w -O $env:TMP\s.wav
