700f14dc25c8a8df3c1509a519232fa048641a15
Heffrey13 <91379359+Heffrey13@users.noreply.github.com>
2022-08-15 00:57:15 -0600

    Update scary.ps1

--- Creepy/assets/scary.ps1
+++ Creepy/assets/scary.ps1
@@ -14,7 +14,7 @@
 
 $RI = Get-Random @(1..3)
 
-$image =  "https://github.com/Heffrey13/Ducky-Pranks/blob/main/Creepy/assets/Wallpaper/eye.png"
+$image =  "https://raw.githubusercontent.com/Heffrey13/Ducky-Pranks/main/Creepy/assets/Wallpaper/eye.png"
 
 $i = -join($image,"?dl=1")
 
